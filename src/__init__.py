cafef56950ea2a09bd422c749520a2605c8aa270
sufyanederes <sosoederes@gmail.com>
2026-06-25 21:13:47 +0800

    Initialize src module package

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Crime Intelligence Dashboard System
+Module for ML-powered crime pattern analysis and classification
+"""
+
+__version__ = "1.0.0"
+__author__ = "Eders Abdalla Alkedr Sufyan"
+__student_id__ = "202402010074"
